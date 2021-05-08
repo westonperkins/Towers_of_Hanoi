a54933c8783afb2f3c837abf089eddcb155e7ed2
westonperkins <westonperkins5@gmail.com>
2021-05-08 13:17:17 -0400

    finishing up MVP

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const towerOne = document.querySelector('.tOne')
 const invisibleTONE = document.querySelector('.IT1')
 const invisibleTTWO = document.querySelector('.IT2')
 const invisibleTTHREE = document.querySelector('.IT3')
+const invis = document.querySelectorAll('.invisible')
 const pillars = document.querySelectorAll('.pillar')
 
 
@@ -9,41 +10,82 @@ const pillars = document.querySelectorAll('.pillar')
 const ringOne = document.createElement('div')
 ringOne.setAttribute("class", 'ring ringOne')
 ringOne.setAttribute("data-ring", '1')
-invisibleTONE.append(ringOne)
 
 const ringTwo = document.createElement('div')
 ringTwo.setAttribute("class", 'ring ringTwo')
 ringTwo.setAttribute("data-ring", '2')
-invisibleTONE.append(ringTwo)
 
 const ringThree = document.createElement('div')
 ringThree.setAttribute("class", 'ring ringThree')
 ringThree.setAttribute("data-ring", '3')
-invisibleTONE.append(ringThree)
 
 const ringFour = document.createElement('div')
 ringFour.setAttribute("class", 'ring ringFour')
 ringFour.setAttribute("data-ring", '4')
-invisibleTONE.append(ringFour)
 
 const ringFive = document.createElement('div')
 ringFive.className = 'ring ringFive'
 ringFive.setAttribute("data-ring", '5')
-invisibleTONE.append(ringFive)
-
-
-
-
 
 
 const rings = document.querySelectorAll('.ring')
 
+// EVENT LISTENER ON RINGS
+
+let towerOneArr = [ringOne, ringTwo, ringThree, ringFour, ringFive]
+let towerTwoArr = []
+let towerThreeArr = []
+
+function ringSetUp(tower, ring){
+    tower.append(ring)
+}
+
+ringSetUp(invisibleTONE, ringOne)
+ringSetUp(invisibleTONE, ringTwo)
+ringSetUp(invisibleTONE, ringThree)
+ringSetUp(invisibleTONE, ringFour)
+ringSetUp(invisibleTONE, ringFive)
+
+let selectedRing = null
+
+function setRing(ring) {
+    ring.style.transform = 'translateY(-40vh)'
+}
+function resetRing(ring) {
+    ring.style.transform = 'translateY(0vh)'
+}
+
+function moveRings(ring, towerTo) {
+    towerTo.append(ring)
+}
+
+// able to move rings (NOT DONE)
+
+for(let i = 0; i < invis.length; i++) {
+    invis[i].addEventListener('click', e => {
+        if(e.target.childNodes.length >= 1) {
+            console.log(e.target.childNodes.length)
+            // console.log(e.target.classList[0])
+            selectedRing = e.target.childNodes[0]
+            setRing(selectedRing)
+            console.log(selectedRing)
+        } else if(e.target.classList[0] == "invisible") {
+            invis[i].addEventListener('click', e => {
+                // moveRings(selectedRing, e.target)
+                // resetRing(selectedRing)
+                // console.log(selectedRing)
+            })
+        }
+    })
+}
+
+
+// moveRings(ringOne, invisibleTTHREE)
+// moveRings(ringTwo, invisibleTTWO)
 
 
 
-
-
-
+// e.target.classList[0] === 'ring'
 
 // RING AND TOWER CLASSES WITH BACKEND LOGIC
 
@@ -107,12 +149,9 @@ const tThree = new Tower('three', [])
 
 
 
-// EVENT LISTENER ON RINGS
 
 
-function moveRings(towerFrom) {
-    towerFrom.style.transform = 'translateY(-40vh)'
-}
+
 
 // for(let i = 0; i < rings.length; i++) {
 //     rings[i].addEventListener('click', e => {
@@ -121,28 +160,28 @@ function moveRings(towerFrom) {
 // }
    
    
-for(let i = 0; i < pillars.length; i++) {
-    pillars[i].addEventListener('click', e => {
-        if(e.target.classList[0] === 'invisible') {
-            console.log(e.target)
-            console.log(e.target.childNodes)
-        }else if(e.target.classList[0] === 'ring') {
-            // WHERE I LEFT OFF: trying to make a condiiton for second click to drop onto second pillar
-            if(e.target.childNodes) {
-                console.log(e.target)
-                const target1 = e.target
-                moveRings(target1)
-            }
-            function placeRings(towerTo) {
-                towerTo.append(target1)
-            }
-            pillars[i].addEventListener('click', e => {
-                placeRings(e.target)
-            })
+// for(let i = 0; i < pillars.length; i++) {
+//     pillars[i].addEventListener('click', e => {
+//         if(e.target.classList[0] === 'invisible') {
+//             console.log(e.target)
+//             console.log(e.target.childNodes)
+//         }else if(e.target.classList[0] === 'ring') {
+//             // WHERE I LEFT OFF: trying to make a condiiton for second click to drop onto second pillar
+//             if(e.target.childNodes) {
+//                 console.log(e.target)
+//                 const target1 = e.target
+//                 moveRings(target1)
+//             }
+//             // function placeRings(towerTo) {
+//             //     towerTo.append(target1)
+//             // }
+//             pillars[i].addEventListener('click', i => {
+//                 placeRings(i.target)
+//             })
   
-        }
-    })
-}
+//         }
+//     })
+// }
 
 
 
